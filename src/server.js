543236cb878cb9f7807c659499222c77ee9bf1ec
felix-824 <nf94new@gmail.com>
2026-05-22 18:47:42 +0900

    TS: Initial Setup

---
+++ src/server.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("EXUCUTED");
+const person = "MARTIN";
+const count = 100;
+//# sourceMappingURL=server.js.map

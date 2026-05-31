77245518b779d4a612754d713fe918591bd01faf
felix-824 <nf94new@gmail.com>
2026-05-31 20:31:03 +0900

    feat: create project standards

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,21 @@ import express from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 
 /** 1-ENTRANCE **/
 const app = express();
 console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));  //form data ni o‘qish
-app.use(express.json());
+app.use(express.urlencoded({extended: true}));  //form data ni o‘qish traditional apIga xizmat
+app.use(express.json());  /// REST api ga xizmat Midlweri
+app.use(morgan(MORGAN_FORMAT));
 
 
 /** 2-SESSIONS **/
 
-/** 3-views **/
+/** 3-views **/                        //BACKEND da frontentni qurish
 app.set("views", path.join(__dirname, "views"));  //EJS filelarni qayerdan qidirishni ko'rsatadi
 app.set("view engine", "ejs"); //HTML sahifani qaysi formatda o‘qishni aytyapti
 
--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!')
+/* Project Standarts:
+   - Logging standards
+   - Naming standatds
+      function, method, variable => CAMEL    goHome
+      class => PASCAL                        MemberService
+      folder => KEBAB
+      css => SNAKE                          button_style
+   - Erorr handling
+      
+ */

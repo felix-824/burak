36a093a0572cf52907cce72a763054a41c5ebb5c
felix-824 <nf94new@gmail.com>
2026-06-06 09:25:18 +0900

    feat: frontend related css, js and image creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -10,7 +10,7 @@ const restauranController: T = {}; //objectga keyinchalik istalgan property qo's
 restauranController.goHome = (req: Request, res: Response) => {
   try {
     console.log('goHome');
-    res.send("Home Page");
+    res.render("home");
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -19,7 +19,7 @@ restauranController.goHome = (req: Request, res: Response) => {
 restauranController.getSignup = (req: Request, res: Response) => {
   try {
     console.log('getSignup');
-    res.send("Signup Page");
+    res.render("signup");
     // send | json | redirect | end | render
   } catch (err) {
     console.log("Error, getSignup:", err);
@@ -29,7 +29,7 @@ restauranController.getSignup = (req: Request, res: Response) => {
 restauranController.getLogin = (req: Request, res: Response) => {
   try {
     console.log('getLogin');
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("Error, getLogin:", err);
   }

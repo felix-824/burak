7a66dd55da5aa54fe55767d1ca959b860da56867
felix-824 <nf94new@gmail.com>
2026-06-20 02:57:32 +0900

    feat: signup process frontent validation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -62,7 +62,7 @@ restauranController.processSignup = async (req: AdminRequest, res: Response) => 
     console.log("Error, processSignup:", err);
       const message = err instanceof Error ? err.message : Message.SOMETHING_WENT_WRONG;
      res.send(
-      `<script> alert("${message}"); window.location.replace('/admin/signup)</script>`);
+      `<script> alert("${message}"); window.location.replace('/admin/signup')</script>`);
   
   }
 };

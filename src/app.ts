bfd4f1194d58b635b5083b451e2870669b828169
felix-824 <nf94new@gmail.com>
2026-07-03 23:57:41 +0900

    feat: develop authentication via tokens with cookies

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
 
 import session from "express-session";
@@ -22,6 +23,7 @@ console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));  //form data ni o‘qish traditional apIga xizmat
 app.use(express.json());  /// REST api ga xizmat Midlweri
+app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT)); // Logging standard
 
 
--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 //REACT
 const memberService = new MemberService();
@@ -17,10 +18,13 @@ memberController.signup = async (req: Request, res: Response) => {
     const input: MemberInput = req.body,
       result: Member = await memberService.signup(input), // call - argument
       token = await authService.createToken(result);
-      console.log('token', token);
-    // TODO: TOKENS AUTHENTICATION
 
-    res.json({member: result});
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 100, 
+      httpOnly: false,
+  });   
+
+     res.status(HttpCode.CREATED).json({member: result, accessToken: token });
   } catch (err) {
     console.log("Error, signup:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -34,10 +38,13 @@ memberController.login = async (req: Request, res: Response) => {
     const input: LoginInput = req.body,
       result = await memberService.login(input),
       token = await authService.createToken(result);
-      console.log("Tokin =>", token);
-     // TODO: TOKENS AUTHENTICATION
+     
+   res.cookie("accessToken", token, {
+    maxAge: AUTH_TIMER * 3600 * 100, 
+    httpOnly: false,
+  });   
 
-     res.json({member: result});
+     res.status(HttpCode.OK).json({member: result, accessToken: token });
   } catch (err) {
     console.log("Error, login:", err);
     if (err instanceof Errors) res.status(err.code).json(err);

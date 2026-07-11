045b095afb2e463026b27d7eac608d2453ca5546
felix-824 <nf94new@gmail.com>
2026-07-11 14:00:11 +0900

    feat: develop createOrder rest Api

---
+++ src/libs/types/order.ts
@@ -0,0 +1,29 @@
+import mongoose, {ObjectId} from "mongoose";
+import { orderStatus } from "../enums/order.enum";
+
+export interface OrderItem {
+    _id: ObjectId;
+    itemQuatity: number;
+    itemPrice: number;
+    orderId: ObjectId;
+    productId: ObjectId;
+    createAt: Date;
+    updatedAt: Date;
+}
+
+export interface Order {
+    _id: ObjectId;
+    orderTotal: number;
+    orderDelivery: number;
+    orderStatus: orderStatus;
+    memberId: ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface OrderItemInput {
+    itemQuantity: number;
+    itemPrice: number;
+    productId: ObjectId;
+    orderId?: ObjectId
+}
